e43108277608ce9ed16a68acc78413d6a15298b5
arnasdill <mageristas12345@gmail.com>
2018-05-24 15:33:15 +0300

    added files

---
+++ code/FingerprintRecognition v2.2/myCustomProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myCustomProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
